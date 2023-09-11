e2d0f60f52e91813382a4045f02638b7dfd1dbb8
joelcr10 <joelcraju@gmail.com>
2023-09-11 13:39:49 +0530

    functions as props

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,6 +1,8 @@
-const BlogList = ({blogs,title}) => {
+const BlogList = ({blogs,title,handleDelete}) => {
     // const blogs = props.blogs;
     // const title = props.title;
+
+    
     return ( 
         <div className="blog-list">
             <h2>{title}</h2>
@@ -8,6 +10,7 @@ const BlogList = ({blogs,title}) => {
                 <div className="blog-preview" key={blog.id}>
                     <h2>{blog.title}</h2>
                     <p>Written by {blog.author}</p>
+                    <button onClick={()=> handleDelete(blog.id)}>delete blog</button>
                 </div>
             ))}
         </div>
--- src/Home.js
+++ src/Home.js
@@ -6,11 +6,17 @@ const Home = () => {
         { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
         { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-      ])
+      ]);
+
+      const handleDelete = (id) =>{
+        const newBlogs = blogs.filter( blog => blog.id!== id);
+        setBlogs(newBlogs);
+      }
     
     return ( 
         <div className="home">
-            <BlogList blogs={blogs} title="All Blogs!"/>
+            <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete}/>
+            {/* <BlogList blogs={blogs.filter((blog)=> blog.author==='mario')} title="Mario's Blog"  /> */}
         </div>
      );
 }
